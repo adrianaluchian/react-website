14bb986a816c84f758825a23cfe4ac0479bb63f0
Adriana Luchian <adriana@Adrianas-MacBook-Pro.local>
2020-01-17 13:27:31 +0100

    Add and configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
+const path = require('path');
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './dist'
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    }]
-  },
-  devtool: 'inline-source-map'
+    entry: './src/index.js',
+    output: {
+        path: path.join([__dirname, '/dist']),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader', 'eslint-loader']
+        }]
+    },
+    devtool: 'inline-source-map'
 };
